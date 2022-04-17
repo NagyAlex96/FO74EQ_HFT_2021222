712c0ec79aba981c9ba61e93cb41fb4c552f3a56
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-17 17:56:12 +0200

    Course Repository created

---
+++ FO74EQ_HFT_2021222.Repository/Model Repository/CourseRespository.cs
@@ -0,0 +1,34 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Database;
+using FO74EQ_HFT_2021222.Repository.Generic_Repository;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Repository.Model_Repository
+{
+    public class CourseRespository : Repository<Course>, IRepository<Course>
+    {
+        public CourseRespository(NeptunDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Course Read(int id)
+        {
+            return ctx.Course.FirstOrDefault(t => t.CourseId == id);
+        }
+
+        public override void Update(Course item)
+        {
+            var old = Read(item.CourseId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
