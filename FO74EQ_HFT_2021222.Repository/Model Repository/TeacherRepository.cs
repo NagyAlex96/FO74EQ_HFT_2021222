e74ccf91db35831e9d54d19f2d97d37a52d57c99
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-17 18:11:27 +0200

    TeacherRepository created

---
+++ FO74EQ_HFT_2021222.Repository/Model Repository/TeacherRepository.cs
@@ -0,0 +1,34 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Database;
+using FO74EQ_HFT_2021222.Repository.Generic_Repository;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Repository.Model_Repository
+{
+    public class TeacherRepository : Repository<Teacher>, IRepository<Teacher>
+    {
+        public TeacherRepository(NeptunDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override Teacher Read(int id)
+        {
+            return ctx.Teacher.FirstOrDefault(t => t.TeacherId == id);
+        }
+
+        public override void Update(Teacher item)
+        {
+            var old = Read(item.TeacherId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
