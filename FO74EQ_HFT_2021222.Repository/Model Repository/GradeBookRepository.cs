afe83b0e0551a59136d8975ede1c627436fc26ad
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-17 17:58:51 +0200

    GradeBookRepository Created

---
+++ FO74EQ_HFT_2021222.Repository/Model Repository/GradeBookRepository.cs
@@ -0,0 +1,34 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Database;
+using FO74EQ_HFT_2021222.Repository.Generic_Repository;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Repository.Model_Repository
+{
+    public class GradeBookRepository : Repository<GradeBook>, IRepository<GradeBook>
+    {
+        public GradeBookRepository(NeptunDbContext ctx) : base(ctx)
+        {
+        }
+
+        public override GradeBook Read(int id)
+        {
+            return ctx.GradeBook.FirstOrDefault(t => t.GradeBookId == id);
+        }
+
+        public override void Update(GradeBook item)
+        {
+            var old = Read(item.GradeBookId);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
