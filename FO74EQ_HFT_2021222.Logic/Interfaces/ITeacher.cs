887cf33d5e829ecb9ad0977c0b308006ee651669
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:14:43 +0200

    ITeacher interface created

---
+++ FO74EQ_HFT_2021222.Logic/Interfaces/ITeacher.cs
@@ -0,0 +1,18 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Interfaces
+{
+    internal interface ITeacher
+    {
+        void Create(Teacher item);
+        void Delete(int id);
+        Teacher Read(int id);
+        IQueryable<Teacher> ReadAll();
+        void Update(Teacher item);
+    }
+}
