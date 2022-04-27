098df27b190136ad6bd627dbbb36d9418ef8c474
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:12:06 +0200

    IGradeBook interface created

---
+++ FO74EQ_HFT_2021222.Logic/Interfaces/IGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Interfaces
+{
+    public interface IGradeBook
+    {
+        void Create(GradeBook item);
+        void Delete(int id);
+        GradeBook Read(int id);
+        IQueryable<GradeBook> ReadAll();
+        void Update(GradeBook item);
+    }
+}
