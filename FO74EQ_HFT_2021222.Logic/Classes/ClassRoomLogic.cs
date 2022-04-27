99fdc3b90ffafeedff20a4c19b84a0d7945c35f6
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:19:09 +0200

    ClassRoomLogic Created

---
+++ FO74EQ_HFT_2021222.Logic/Classes/ClassRoomLogic.cs
@@ -0,0 +1,46 @@
+﻿using FO74EQ_HFT_2021222.Logic.Interfaces;
+using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Classes
+{
+    public class ClassRoomLogic : IClassRoom
+    {
+        IRepository<ClassRoom> repo;
+
+        public ClassRoomLogic(IRepository<ClassRoom> repository)
+        {
+            this.repo = repository;
+        }
+
+        public void Create(ClassRoom item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public ClassRoom Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<ClassRoom> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(ClassRoom item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
