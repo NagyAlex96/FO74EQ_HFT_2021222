d6f80faaa3ad7883e64d66829e59800d72a5c3ff
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:09:53 +0200

    IClassRoom interface created

---
+++ FO74EQ_HFT_2021222.Logic/Interfaces/IClassRoom.cs
@@ -0,0 +1,18 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Interfaces
+{
+    public interface IClassRoom
+    {
+        void Create(ClassRoom item);
+        void Delete(int id);
+        ClassRoom Read(int id);
+        IQueryable<ClassRoom> ReadAll();
+        void Update(ClassRoom item);
+    }
+}
