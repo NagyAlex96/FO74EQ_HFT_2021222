e8ae3f6ef5da6b879a6d1a6db49336f374714614
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:28:39 +0200

    TeacherLogic Created

---
+++ FO74EQ_HFT_2021222.Logic/Classes/TeacherLogic.cs
@@ -0,0 +1,46 @@
+﻿using FO74EQ_HFT_2021222.Logic.Interfaces;
+using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Classes
+{
+    public class TeacherLogic : ITeacher
+    {
+        IRepository<Teacher> repo;
+
+        public TeacherLogic(IRepository<Teacher> repository)
+        {
+            this.repo = repository;
+        }
+
+        public void Create(Teacher item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Teacher Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Teacher> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Teacher item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
