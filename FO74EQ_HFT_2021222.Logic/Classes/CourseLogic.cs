73efe02bba03ea5f26a03ac5351585f1a5c4e472
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:23:42 +0200

    Course & GradeBook Logic created

---
+++ FO74EQ_HFT_2021222.Logic/Classes/CourseLogic.cs
@@ -0,0 +1,46 @@
+﻿using FO74EQ_HFT_2021222.Logic.Interfaces;
+using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Classes
+{
+    public class CourseLogic : ICourse
+    {
+        IRepository<Course> repo;
+
+        public CourseLogic(IRepository<Course> repository)
+        {
+            this.repo = repository;
+        }
+
+        public void Create(Course item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Course Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<Course> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Course item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
---
+++ FO74EQ_HFT_2021222.Logic/Classes/GradeBookLogic.cs
@@ -0,0 +1,46 @@
+﻿using FO74EQ_HFT_2021222.Logic.Interfaces;
+using FO74EQ_HFT_2021222.Models;
+using FO74EQ_HFT_2021222.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Classes
+{
+    public class GradeBookLogic : IGradeBook
+    {
+        IRepository<GradeBook> repo;
+
+        public GradeBookLogic(IRepository<GradeBook> repository)
+        {
+            this.repo = repository;
+        }
+
+        public void Create(GradeBook item)
+        {
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public GradeBook Read(int id)
+        {
+            return this.repo.Read(id);
+        }
+
+        public IQueryable<GradeBook> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(GradeBook item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
