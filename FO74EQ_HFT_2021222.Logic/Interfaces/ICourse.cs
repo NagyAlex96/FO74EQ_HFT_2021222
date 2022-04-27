2ea5b8b738d06b9502f13a5354ebb36a5da194f8
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-27 11:10:53 +0200

    ICourse interface created

---
+++ FO74EQ_HFT_2021222.Logic/Interfaces/ICourse.cs
@@ -0,0 +1,18 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.Logic.Interfaces
+{
+    public interface ICourse
+    {
+        void Create(Course item);
+        void Delete(int id);
+        Course Read(int id);
+        IQueryable<Course> ReadAll();
+        void Update(Course item);
+    }
+}
