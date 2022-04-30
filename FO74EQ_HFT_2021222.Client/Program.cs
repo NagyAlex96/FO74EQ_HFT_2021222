2ce1c78bb923785e7e5c44508fd74d142a7cd752
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-30 23:44:01 +0200

    Client Create done

--- FO74EQ_HFT_2021222.Client/Program.cs
+++ FO74EQ_HFT_2021222.Client/Program.cs
@@ -1,12 +1,237 @@
-﻿using System;
+﻿using ConsoleTools;
+using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FO74EQ_HFT_2021222.Client
 {
     internal class Program
     {
+        static RestService rest;
+
+        static void Create(string entity)
+        {
+            switch (entity.ToLower())
+            {
+                case "student":
+                    Console.Write("Enter Student NeptunId: ");
+                    int neptunId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Student First Name: ");
+                    string firstName = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Student Last Name: ");
+                    string lastName = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Student Email Address: ");
+                    string email = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Student DateOfBirth Name: ");
+                    DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+
+                    rest.Post(new Student()
+                    {
+                        NeptunId = neptunId,
+                        FirstName = firstName,
+                        LastName = lastName,
+                        DateOfBirth = dateOfBirth,
+                        Email = email
+                    }, "student");
+                    break;
+
+                case "teacher":
+                    Console.Write("Enter TeacherId NeptunId: ");
+                    int teacherId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Teacher First Name: ");
+                    string firstNameTeacher = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Teacher Last Name: ");
+                    string lastNameTeacher = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Teacher salary: ");
+                    int salary = int.Parse(Console.ReadLine());
+
+                    rest.Post(new Teacher()
+                    {
+                        TeacherId = teacherId,
+                        FirstName = firstNameTeacher,
+                        LastName = lastNameTeacher,
+                        Salary = salary,
+                    }, "teacher");
+                    break;
+
+                case "gradebook":
+                    Console.Write("Enter GradeBook Id: ");
+                    int gradeBookId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter GradeBook NeptunId: ");
+                    int neptunIdGrade = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter GradeBook TeacherId: ");
+                    int teacherIdGrade = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter GradeBook teacherId: ");
+                    int courseIdGrade = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter GradeBook grade: ");
+                    int grade = int.Parse(Console.ReadLine());
+                    
+                    Console.WriteLine("\nEnter GradeBook rating: ");
+                    int rating = int.Parse(Console.ReadLine());
+
+                    rest.Post(new GradeBook()
+                    {
+                        GradeBookId = gradeBookId,
+                        NeptunId = neptunIdGrade,
+                        TeacherId = teacherIdGrade,
+                        CourseId = courseIdGrade,
+                        Grade = grade,
+                        Rating = rating,
+
+                    }, "gradeBook");
+                    break;
+
+                case "course":
+                    Console.Write("Enter Course Id: ");
+                    int courseId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Course name: ");
+                    string name = Console.ReadLine();
+
+                    Console.WriteLine("\nEnter Course date of announce: ");
+                    DateTime dateOfAnnounce = DateTime.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Course credit: ");
+                    int credit = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Course class room Id: ");
+                    int classRoomIdCourse = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter Course requirementId: ");
+                    int requireMentID = int.Parse(Console.ReadLine());
+
+                    rest.Post(new Course()
+                    {
+                        CourseId = courseId,
+                        Name = name,
+                        DateOfAnnounced = dateOfAnnounce,
+                        Credit = credit,
+                        ClassRoomId = classRoomIdCourse,
+                        RequirementId = requireMentID,
+
+                    }, "Course");
+                    break;
+
+                case "classroom":
+                    Console.Write("Enter class room Id: ");
+                    int classRoomId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nEnter class room capacity: ");
+                    int capacity = int.Parse(Console.ReadLine());
+
+
+                    rest.Post(new ClassRoom()
+                    {
+                        ClassRoomId = classRoomId,
+                        Capacity = capacity,
+
+                    }, "classRoom");
+                    break;
+
+                default:
+                    throw new Exception("Unknown error");
+                    break;
+            }
+        }
+
+        static void List(string entity)
+        {
+            if (entity == "Student")
+            {
+                List<Student> actors = rest.Get<Student>("student");
+                foreach (var item in actors)
+                {
+                    Console.WriteLine(item.NeptunId + ": " + $"{item.FirstName}{item.LastName}");
+                }
+            }
+            Console.ReadLine();
+        }
+        static void Update(string entity)
+        {
+            if (entity == "Actor")
+            {
+                Console.Write("Enter Actor's id to update: ");
+                int id = int.Parse(Console.ReadLine());
+                Student one = rest.Get<Student>(id, "student");
+                Console.Write($"New name [old: {one.FirstName}{one.LastName}]: ");
+                string name = Console.ReadLine();
+                one.FirstName = name;
+                rest.Put(one, "actor");
+            }
+        }
+        static void Delete(string entity)
+        {
+            if (entity == "Actor")
+            {
+                Console.Write("Enter Actor's id to delete: ");
+                int id = int.Parse(Console.ReadLine());
+                rest.Delete(id, "actor");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            rest = new RestService("http://localhost:48036/", "GradeBook");
+
+            var classRoomSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("ClassRoom"))
+                .Add("Create", () => Create("ClassRoom"))
+                .Add("Delete", () => Delete("ClassRoom"))
+                .Add("Update", () => Update("ClassRoom"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var courseSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Course"))
+                .Add("Create", () => Create("Course"))
+                .Add("Delete", () => Delete("Course"))
+                .Add("Update", () => Update("Course"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var gradeBookSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("GradeBook"))
+                .Add("Create", () => Create("GradeBook"))
+                .Add("Delete", () => Delete("GradeBook"))
+                .Add("Update", () => Update("GradeBook"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var studentSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Student"))
+                .Add("Create", () => Create("Student"))
+                .Add("Delete", () => Delete("Student"))
+                .Add("Update", () => Update("Student"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var teacherSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Teacher"))
+                .Add("Create", () => Create("Teacher"))
+                .Add("Delete", () => Delete("Teacher"))
+                .Add("Update", () => Update("Teacher"))
+                .Add("Exit", ConsoleMenu.Close);
+
+
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Students", () => studentSubMenu.Show())
+                .Add("ClassRooms", () => classRoomSubMenu.Show())
+                .Add("Courses", () => courseSubMenu.Show())
+                .Add("GradeBooks", () => gradeBookSubMenu.Show())
+                .Add("Teachers", () => teacherSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+
+            menu.Show();
+
         }
     }
 }
--- FO74EQ_HFT_2021222.Endpoint/Startup.cs
+++ FO74EQ_HFT_2021222.Endpoint/Startup.cs
@@ -1,10 +1,13 @@
 using FO74EQ_HFT_2021222.Logic.Classes;
 using FO74EQ_HFT_2021222.Logic.Interfaces;
+using FO74EQ_HFT_2021222.Models;
 using FO74EQ_HFT_2021222.Repository.Database;
 using FO74EQ_HFT_2021222.Repository.Interfaces;
 using FO74EQ_HFT_2021222.Repository.Model_Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,21 +19,25 @@ namespace FO74EQ_HFT_2021222.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddTransient<NeptunDbContext>();
 
             services.AddTransient<IClassRoomLogic, ClassRoomLogic>();
-            services.AddTransient<ClassRoomRepository, ClassRoomRepository>(); //TODO ez így jó lesz?  
-            
-            services.AddTransient<ICourseLogic, CourseLogic>();
-            services.AddTransient<CourseRespository, CourseRespository>();            
-            services.AddTransient<IGradeBookLogic, GradeBookLogic>();
-            services.AddTransient<GradeBookRepository, GradeBookRepository>();            
-            services.AddTransient<IStudentLogic, StudentLogic>();
-            services.AddTransient<StudentRepository, StudentRepository>();            
-            services.AddTransient<ITeacherLogic, TeacherLogic>();
-            services.AddTransient<TeacherRepository, TeacherRepository>();
+            services.AddTransient<IRepository<ClassRoom>, ClassRoomRepository>(); 
 
-            services.AddTransient<NeptunDbContext, NeptunDbContext>();
+            services.AddTransient<ICourseLogic, CourseLogic>();
+            services.AddTransient<IRepository<Course>, CourseRespository>();
+
+            services.AddTransient<IGradeBookLogic, GradeBookLogic>();
+            services.AddTransient<IRepository<GradeBook>, GradeBookRepository>();
+
+            services.AddTransient<IStudentLogic, StudentLogic>();
+            services.AddTransient<IRepository<Student>, StudentRepository>();
+
+            services.AddTransient<ITeacherLogic, TeacherLogic>();
+            services.AddTransient<IRepository<Teacher>, TeacherRepository>();
+
+            services.AddControllers();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,32 +45,33 @@ namespace FO74EQ_HFT_2021222.Endpoint
         //8.fejezet 10.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
             if (env.IsDevelopment())
             {
+                //TODO error
                 app.UseDeveloperExceptionPage();
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieDbApp.Endpoint v1"));
             }
 
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
+
+
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
