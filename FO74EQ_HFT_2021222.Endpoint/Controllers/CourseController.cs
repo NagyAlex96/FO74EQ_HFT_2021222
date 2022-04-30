2653c3e06ac6015ffb307d8eb75b970346c64a28
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-04-30 22:28:51 +0200

    CourseController created

--- FO74EQ_HFT_2021222.Endpoint/Controllers/CourseController.cs
+++ FO74EQ_HFT_2021222.Endpoint/Controllers/CourseController.cs
@@ -6,39 +6,38 @@ using System.Collections.Generic;
 
 namespace FO74EQ_HFT_2021222.Endpoint.Controllers
 {
-    public class ClassRoomController : Controller
+    public class CourseController : Controller
     {
-        IClassRoom logic;
-
-        public ClassRoomController(IClassRoom classRoom)
+		ICourse logic;
+        public CourseController(ICourse logic)
         {
-            this.logic = classRoom;
+            this.logic = logic;
         }
 
-		// GET: 
-		[HttpGet]
-		public IEnumerable<ClassRoom> Get()
+        // GET: 
+        [HttpGet]
+		public IEnumerable<Course> Get()
 		{
 			return logic.ReadAll();
 		}
 
 		// GET: one
 		[HttpGet("{id}")]
-		public ClassRoom Get(int id)
+		public Course Get(int id)
 		{
 			return logic.Read(id);
 		}
 
 		// POST:
 		[HttpPost]
-		public void Post([FromBody] ClassRoom value)
+		public void Post([FromBody] Course value)
 		{
 			logic.Create(value);
 		}
 
 		// PUT:
 		[HttpPut("{id}")]
-		public void Put([FromBody] ClassRoom value)
+		public void Put([FromBody] Course value)
 		{
 			logic.Update(value);
 		}
