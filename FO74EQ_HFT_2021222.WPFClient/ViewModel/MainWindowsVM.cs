5c7d68b727c062191a5255bbeaacacd4e594a707
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-11-12 19:22:26 +0100

    ViewModel Created

---
+++ FO74EQ_HFT_2021222.WPFClient/ViewModel/MainWindowsVM.cs
@@ -0,0 +1,20 @@
+﻿using FO74EQ_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FO74EQ_HFT_2021222.WPFClient.ViewModel
+{
+    public class MainWindowsVM
+    {
+        public MainWindowsVM()
+        {
+            Students = new RestCollection<Student>("http://localhost:48036/","Student");
+        }
+
+        public RestCollection<Student> Students { get; set; }
+
+    }
+}
