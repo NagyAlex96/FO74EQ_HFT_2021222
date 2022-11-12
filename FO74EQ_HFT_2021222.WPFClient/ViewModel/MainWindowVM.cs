0e91c243c381884b50fad1b8c5b42bf109f2b746
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-11-12 19:08:53 +0100

    Commands created (Delete, Update, Create)

--- FO74EQ_HFT_2021222.WPFClient/ViewModel/MainWindowVM.cs
+++ FO74EQ_HFT_2021222.WPFClient/ViewModel/MainWindowVM.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FO74EQ_HFT_2021222.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace FO74EQ_HFT_2021222.WPFClient.ViewModel
 {
-    public class MainWindowVM
+    public class MainWindowVM : ObservableRecipient
     {
+        public MainWindowVM()
+        {
+            Students = new RestCollection<Student>("http://localhost:48036/", "Student");
+
+            CreateStudentCommand = new RelayCommand(CreateStudent);
+            DeleteStudentCommand = new RelayCommand(DeleteStudent);
+            UpdateStudentCommand = new RelayCommand(UpdateStudent);
+        }
+        public RestCollection<Student> Students { get; set; }
+
+        public IRelayCommand CreateStudentCommand { get; private set; }
+        private void CreateStudent()
+        {
+
+        }        
+        
+        public IRelayCommand DeleteStudentCommand { get; private set; }
+        private void DeleteStudent()
+        {
+
+        }       
+        
+        public IRelayCommand UpdateStudentCommand { get; private set; }
+        private void UpdateStudent()
+        {
+
+        }
+
+
     }
 }
