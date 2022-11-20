8f97e73f02ac453c959a60b9d18f5d9fab73fbf5
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-11-20 19:38:54 +0100

    javaScript create in progress

---
+++ FO74EQ_HFT_202122.JSClient/WWWRoot/Script.js
@@ -0,0 +1,27 @@
+﻿let student = [];
+
+//launcSettings.json fájl urljével nem műkődik.
+fetch('http://localhost:48036/Student')
+    .then(x => x.json())
+    .then(y =>
+    {
+        student = y
+        console.log(student);
+        Display();
+    });
+
+function Display() {
+    student.forEach(t =>
+    {
+        document.getElementById('resultArea').innerHTML +=
+            "<tr><td>" + t.neptunId + "</td>" +
+            "<td>" + t.firstName + "</td>" +
+            "<td>" + t.lastName + "</td>" +
+            "<td>" + t.dateOfBirth + "</td>" +
+            "<td>" + t.email + "</td></tr>";
+    });
+}
+
+function Create() {
+    let name = document.getElementById('neptunid').value;
+}
