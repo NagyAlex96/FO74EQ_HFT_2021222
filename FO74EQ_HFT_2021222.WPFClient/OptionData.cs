f011d008c489f980646868618e6677bd81b162fb
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-11-20 11:14:07 +0100

    View Update

---
+++ FO74EQ_HFT_2021222.WPFClient/OptionData.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FO74EQ_HFT_2021222.WPFClient.ViewModel
+{
+    public class OptionData : ObservableObject
+    {
+		private string optionName;
+
+		public string OptionName
+        {
+			get { return optionName; }
+			set { SetProperty(ref optionName, value); }
+		}
+
+		private Visibility visibility;
+
+		public Visibility Visibility
+		{
+			get { return visibility; }
+			set { SetProperty(ref visibility, value); }
+		}
+
+
+	}
+}
